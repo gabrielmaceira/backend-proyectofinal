dc5516aaab2c4ce32d91c8e95b88c04aa6e04947
Gabriel Maceira Alvarez <gabrielmaceira@gmail.com>
2022-03-30 19:36:14 -0300

    updated urls again

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const server = app.listen(PORT, () => {
 
 server.on("error", error => console.log(`Error en servidor ${error}`))
 
-const APIPRODROUTE = '/productos'
-const APICARTROUTE = '/carrito'
+const APIPRODROUTE = '/api/productos'
+const APICARTROUTE = '/api/carrito'
 
 app.use(APIPRODROUTE, route_products)
 app.use(APICARTROUTE, route_cart)
